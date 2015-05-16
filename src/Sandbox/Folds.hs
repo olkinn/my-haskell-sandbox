30b6be6da65b73e6d06fd9052b92b19c70feb097
Olya Piatrova <olkinn@ymail.com>
2015-05-17 00:08:52 +0300

    added last'

--- src/Sandbox/Folds.hs
+++ src/Sandbox/Folds.hs
@@ -1,4 +1,7 @@
-module Sandbox.Folds (sum')  where
+module Sandbox.Folds (sum', last')  where
 
 sum' :: (Num a) => [a] -> a  
 sum' xs = foldl (\acc x -> acc + x) 0 xs  
+
+last' :: [a] -> a
+last' = foldl1 (\acc x -> x) 
--- test/Sandbox/FoldsSpec.hs
+++ test/Sandbox/FoldsSpec.hs
@@ -10,3 +10,8 @@ spec = do
 
     it "Calculates the sum of the elements of a list" $ do
       sum' [3, 2, 1, 10] `shouldBe` 16
+
+  describe "last'" $ do
+
+	it "returns the last element of a list" $ do
+		last' [4,5,3,8] `shouldBe` 8
