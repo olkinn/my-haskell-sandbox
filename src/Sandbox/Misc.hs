1dd996ba05b465e0dab02fc3efa1be47eb4a3b28
Olya Piatrova <olkinn@ymail.com>
2015-05-16 22:25:46 +0300

    optimized working code

--- src/Sandbox/Misc.hs
+++ src/Sandbox/Misc.hs
@@ -1,11 +1,11 @@
 module Sandbox.Misc (matrixMaxMin)  where
 
 matrixMaxMin :: [[Int]] -> Int
-matrixMaxMin matrix = max (listValues matrix)
+matrixMaxMin matrix = maximum (listValues matrix)
 
 matrixMin :: (Ord a)=> [a] -> a
 matrixMin [] = error "min called on an empty list"
-matrixMin  = foldr1 (\x y -> if x < y then x else y)
+matrixMin xs = foldr1 (\x y -> if x < y then x else y) xs
 
 listValues :: [[Int]] -> [Int]
 listValues =  map matrixMin
